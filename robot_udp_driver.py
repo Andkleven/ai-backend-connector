291241f5fcbfd6aa0ba5b9c8e34f1a5576813782
Sami J. Lehtinen <sjl@iki.fi>
2020-08-29 21:43:54 +0300

    Control of robot over UDP.

--- robot_udp_driver.py
+++ robot_udp_driver.py
@@ -1,13 +1,29 @@
+"""
+Usage: robot_udp_driver.py [options] LEFT RIGHT
+
+Options:
+    --help  This help.
+    --address ADDRESS  Address to connect. [default: 192.168.1.199]
+    --port PORT        Port to use. [default: 50052]
+"""
+
 import proto.RobotSystemCommunication_pb2 as rsc_pb2
 import socket
+import docopt
 
-UDP_IP = "192.168.1.142"
+
+UDP_IP = "192.168.1.199"
 UDP_PORT = 50052
 
+
+
 if __name__ == "__main__":
+    opts = docopt.docopt(__doc__)
+
     motor_values = rsc_pb2.RobotActionRequest(
-        leftMotorAction=200,
-        rightMotorAction=-400)
+        leftMotorAction=int(opts['LEFT'].replace('~', '-')),
+        rightMotorAction=int(opts['RIGHT'].replace('~', '-')))
     sock = socket.socket(socket.AF_INET, # Internet
                         socket.SOCK_DGRAM) # UDP
-    sock.sendto(motor_values.SerializeToString(), (UDP_IP, UDP_PORT))
+    sock.sendto(motor_values.SerializeToString(), (opts['--address'], int(opts['--port'])))
+    print("bar")
