07b5feeb448a114863c64764fc1444801cc4e710
Sami J. Lehtinen <sjl@iki.fi>
2020-09-13 14:46:58 +0300

    Moved protocol master code to ai-proto.
    robot_udp_driver.py can take advantage of the timeout field in the protocol.

--- robot_udp_driver.py
+++ robot_udp_driver.py
@@ -1,12 +1,16 @@
 """
-Usage: robot_udp_driver.py [options] LEFT RIGHT
+Usage: robot_udp_driver.py [options] motor [--timeout=TIMEOUT] LEFT RIGHT
 
 Options:
     --help  This help.
     --address ADDRESS  Address to connect. [default: 192.168.1.199]
     --port PORT        Port to use. [default: 50052]
+    --timeout=TIMEOUT  Timeout for motor command in milliseconds.
+                       [default: 200]
 """
-
+import sys
+import os.path
+sys.path.append(os.path.dirname(__file__))
 import proto.RobotSystemCommunication_pb2 as rsc_pb2
 import socket
 import docopt
@@ -16,14 +20,15 @@ UDP_IP = "192.168.1.199"
 UDP_PORT = 50052
 
 
-
 if __name__ == "__main__":
     opts = docopt.docopt(__doc__)
 
-    motor_values = rsc_pb2.RobotActionRequest(
-        leftMotorAction=int(opts['LEFT'].replace('~', '-')),
-        rightMotorAction=int(opts['RIGHT'].replace('~', '-')))
+    motor_values = rsc_pb2.RobotRequest(
+        reqId=1,
+        act=rsc_pb2.RobotActionRequest(
+                    leftMotorAction=int(opts['LEFT'].replace('~', '-')),
+                    rightMotorAction=int(opts['RIGHT'].replace('~', '-')),
+                    actionTimeout=int(opts['--timeout'])))
     sock = socket.socket(socket.AF_INET, # Internet
                         socket.SOCK_DGRAM) # UDP
     sock.sendto(motor_values.SerializeToString(), (opts['--address'], int(opts['--port'])))
-    print("bar")
