9ee6a9392f86f630178e4a6806c4ef9e438c0931
andkleven <amkleven@gmail.com>
2021-01-09 20:27:26 +0100

    gui

--- gui_robot_backend.py
+++ gui_robot_backend.py
@@ -85,16 +85,8 @@ def setup_variables(mode):
 
 
 def print_game_data(shared_data):
-    console_text = \
-        f'Status: {shared_data["status"]} \n' \
-        f'Real FPS: \t\t{shared_data["actualDurationFPS"]:4.1f}FPS \n' \
-        f'Real process time: \t{shared_data["actualDuration"]*1000:5.0f}ms\n' \
-        f'Limited FPS: \t\t{shared_data["totalDurationFPS"]:4.1f}FPS \n' \
-        f'Limited process time: \t{shared_data["totalDuration"]*1000:5.0f}ms \n' \
-        f'Image cap dur: \t\t{shared_data["imageCaptureDuration"]*1000:5.0f}ms \n' \
-        f'Obs dur: \t\t{shared_data["obsCreationDuration"]*1000:5.0f}ms \n' \
-        f'Brain dur: \t\t{shared_data["brainDuration"]*1000:5.0f}ms \n' \
-        f'Frontend dur: \t\t{shared_data["frontendDuration"]*1000:5.0f}ms'
+    console_text = f'Status: {shared_data["status"]} \n' + f'Real FPS: \t\t{shared_data["actualDurationFPS"]:4.1f}FPS \n' + f'Real process time: \t{shared_data["actualDuration"]*1000:5.0f}ms\n' + f'Limited FPS: \t\t{shared_data["totalDurationFPS"]:4.1f}FPS \n' + f'Limited process time: \t{shared_data["totalDuration"]*1000:5.0f}ms \n' + f'Image cap dur: \t\t{shared_data["imageCaptureDuration"]*1000:5.0f}ms \n' + f'Obs dur: \t\t{shared_data["obsCreationDuration"]*1000:5.0f}ms \n' + f'Brain dur: \t\t{shared_data["brainDuration"]*1000:5.0f}ms \n' + f'Frontend dur: \t\t{shared_data["frontendDuration"]*1000:5.0f}'
+
     line_jumps = console_text.count('\n')+2
     print(console_text)
     print(cursor_up(line_jumps))
--- hello.py
+++ hello.py
@@ -19,7 +19,16 @@ shared_data = {
         "upperObs": [],
         "angles": []
     }
-console_text = f'Status: {shared_data["status"]} \n' + f'Real FPS: \t\t{shared_data["actualDurationFPS"]:4.1f}FPS \n' + f'Real process time: \t{shared_data["actualDuration"]*1000:5.0f}ms\n' + f'Limited FPS: \t\t{shared_data["totalDurationFPS"]:4.1f}FPS \n' + f'Limited process time: \t{shared_data["totalDuration"]*1000:5.0f}ms \n' + f'Image cap dur: \t\t{shared_data["imageCaptureDuration"]*1000:5.0f}ms \n' + f'Obs dur: \t\t{shared_data["obsCreationDuration"]*1000:5.0f}ms \n' + f'Brain dur: \t\t{shared_data["brainDuration"]*1000:5.0f}ms \n' + f'Frontend dur: \t\t{shared_data["frontendDuration"]*1000:5.0f}'
+console_text = \
+f'Status: {shared_data["status"]} \n' \
+f'Real FPS: \t\t{shared_data["actualDurationFPS"]:4.1f}FPS \n' \
+f'Real process time: \t{shared_data["actualDuration"]*1000:5.0f}ms\n' \
+f'Limited FPS: \t\t{shared_data["totalDurationFPS"]:4.1f}FPS \n' \
+f'Limited process time: \t{shared_data["totalDuration"]*1000:5.0f}ms \n' \
+f'Image cap dur: \t\t{shared_data["imageCaptureDuration"]*1000:5.0f}ms \n' \
+f'Obs dur: \t\t{shared_data["obsCreationDuration"]*1000:5.0f}ms \n' \
+f'Brain dur: \t\t{shared_data["brainDuration"]*1000:5.0f}ms \n' \
+f'Frontend dur: \t\t{shared_data["frontendDuration"]*1000:5.0f}ms'
 line_jumps = console_text.count('\n')+2
 print(console_text)
 print(cursor_up(line_jumps))
